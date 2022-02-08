71832d2413f7c72344857c303628db1db4a65058
Anshika Agarwal <agarwal.anshika100@gmail.com>
2022-02-08 22:38:53 +0530

    made the whole site responsive

--- css/layout/sidebar.js
+++ css/layout/sidebar.js
@@ -1,11 +1,15 @@
-let dropdown = document.getElementsByClassName("mobile-sidebar-icon")[0];
-let mobileSidebar = document.getElementsByClassName("side-bar")[0];
-let components= document.querySelectorAll(".side-bar-links");
-dropdown.onclick = ()=>{
-    mobileSidebar.classList.toggle("side-bar-active");
+let ham= document.querySelector(".nav-hamburger");
+let side= document.querySelector(".side-bar");
+ham.onclick=()=>{
+side.classList.toggle("sidebar-active");
 }
-for(i=0;i<components.length;i++){
-    components[i].addEventListener("click",()=>{
-        mobileSidebar.classList.toggle("side-bar-active");
-    })
+
+/************************* */
+let sidebarlink= document.querySelectorAll(".side-bar-links");
+
+for(let i=0;i<sidebarlink.length;i++){
+    sidebarlink[i].addEventListener("click",()=>{
+        side.classList.toggle("sidebar-active");
+    }
+        )
 }
